1c5d38914a062a1d55407fc958f9ed093d77d3be
Arturas <99223804+arturasVis@users.noreply.github.com>
2023-10-29 21:04:10 +0000

    Exercise 4

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let wordLength=word.length
+    let newWord=""
+    for(let i=wordLength-1;i>=0;i--)
+    {
+        newWord+=word[i]
+    }
+    return newWord
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,23 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,...manyMoreArgs) {
+    let newArray=array
+    let indexOf,a,b
+    for(let i=0;i<manyMoreArgs.length;i++)
+    {
+        a=manyMoreArgs[i]
+        for(let k=0;k<array.length;k++)
+        {
+            b=array[k]
+            if(a===b)
+            {
+                indexOf=array.indexOf(a)
+                if(indexOf>-1)
+                    newArray.splice(indexOf,1)
+            }
+        }
+        
+    }
 
+    return newArray
 };
 
 // Do not edit below this line
