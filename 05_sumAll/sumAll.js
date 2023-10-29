42aeb57c64df8a74b6efba9b84558b12eeb975b2
Arturas <99223804+arturasVis@users.noreply.github.com>
2023-10-29 21:19:04 +0000

    Finished 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(num1,num2) {
+    if(num1<0 || num2 <0)
+        return "ERROR"
+    let temp
+    if(num1>num2)
+    {
+        temp=num1
+        num1=num2
+        num2=temp
+    }
+    function isArray(number) {
+        return number.constructor===Number;
+      }
+    if(!isArray(num1) || !isArray(num2))
+        return "ERROR"    
+    let sum=0
+    for(let i=num1;i<=num2;i++)
+        sum+=i
+    return sum
 };
 
 // Do not edit below this line
