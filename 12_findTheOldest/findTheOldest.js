24be14f831cf7210220ce546d6c5c41e4616c0d9
Arturas <99223804+arturasVis@users.noreply.github.com>
2023-11-05 19:26:17 +0000

    11 and 12 finished

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const oldesPerson=people.reduce((a,b)=>{
+        if(!b["yearOfDeath"])
+            b["yearOfDeath"]=(new Date().getFullYear())
+        if(a.yearOfDeath-a.yearOfBirth<b.yearOfDeath-b.yearOfBirth)
+            a=b
+        return a
+    },{name:"",yearOfBirth:0,yearOfDeath:0})
+    return oldesPerson
 };
 
 // Do not edit below this line
