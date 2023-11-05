887516611a55d85c5e53b98aba1257fb8bd94a51
Arturas <99223804+arturasVis@users.noreply.github.com>
2023-11-05 19:00:56 +0000

    finished 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(fibPos) {
+    let fibNumber=[0,1]
+    if(fibPos<0)
+        return "OOPS"
+    for(let i=2;i<=fibPos;i++)
+    {
+        const nextNumber=fibNumber[i-1]+fibNumber[i-2]
+        fibNumber[i]=nextNumber
+    }
+    return fibNumber[fibPos]
 };
 
 // Do not edit below this line
